b993712fc82b82d1e7829f1c415571ff0f4cb7c6
POWER <POWER@DESKTOP-RIEUAO5>
2022-01-21 10:26:17 +0900

    dd

--- 26_Last/src/last/service/CommonService.java
+++
@@ -1,8 +0,0 @@
-package last.service;
-
-import javafx.event.ActionEvent;
-
-public interface CommonService {
-	public void windowClose(ActionEvent event);
-	public void errorMsg(String title, String headerStr, String contextStr);
-}
